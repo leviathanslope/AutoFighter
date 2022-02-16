318941fdddb7813cc69d8d301de5026a50edb1c5
Adam <Adamqplett@gmail.com>
2022-02-16 00:19:40 -0600

    Added MainMenu and GameState scenes, Scripts, Fonts, and Sprite
    Added Scenes with menu UIs and scripts, fonts, and sprites to make it work/ look good

---
+++ Assets/Scripts/GameStateController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameStateController : MonoBehaviour
+{
+    [SerializeField] GameObject _MenuPanel;
+    [SerializeField] GameObject _SettingsPanel;
+    void Update()
+    {
+        //brings up pop up menu
+        if ((Input.GetKeyDown(KeyCode.Escape)) && (_MenuPanel.activeSelf == false))
+        {
+            PopUpMenu();
+        }
+        else if ((Input.GetKeyDown(KeyCode.Escape)) && (_MenuPanel.activeSelf == true))
+        {
+            Resume();
+        }
+    }
+    public void ExitLevel()
+    {
+        //load main menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PopUpMenu()
+    {
+        //makes pop up menu visible
+        _MenuPanel.SetActive(true);
+    }
+    public void Resume()
+    {
+        //makes pop up menu disappear
+        _MenuPanel.SetActive(false);
+        //if settings menu is up it also closes
+        _SettingsPanel.SetActive(false);
+    }
+}
