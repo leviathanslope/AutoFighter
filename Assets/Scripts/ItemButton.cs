5753e6ca757a7837125dd035b3238a05a2fc48a5
Casie <kcwilford@hotmail.com>
2022-03-02 21:18:23 -0600

    Store

---
+++ Assets/Scripts/ItemButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemButton : MonoBehaviour
+{
+    public Text inventoryNotif;
+
+    public void addItem()
+    {
+        inventoryNotif.text = this.name + " was added to inventory.";
+        gameObject.SetActive(false);
+    }
+}
